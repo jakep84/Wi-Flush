cb821d378720cd017699d9b2f9d5d8623d975be9
Jake Priddy <priddyjacob84@gmail.com>
2016-09-03 12:11:25 -0400

    board up light blinking on pin 13

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 var five = require('johnny-five'),
 	board = new five.Board(),
-	led,
-	toggleState = false;
-//create the board object
-board.on("ready", function () {
-	//acknowledge board is ready
-	console.log('Board ready');
-	//create an led 
+	led
+
+	board.on("ready", function () {
+	console.log('Board is ready');
+		
 	led = new five.Led(13);
-	//run function every 2 seconds
-	setInterval(toggleLED, 200);
+	console.log('LED is set');
 	
-	function toggleLED() {
-		toggleState = !toggleState;
-		//toggle led light on if off and off if on 
-		if (toggleState) led.on();
-		else led.off():
-	}
-});
+	led.blink(5000);
+	console.log('LED toggle on/off e5s');
+
+	});
 //program is up and running
 console.log("\nWaiting for device to connect...");
