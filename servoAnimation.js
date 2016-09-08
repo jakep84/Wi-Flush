49f8906e2701ceaabb48ed3d366ddf6f2b575c0d
Jake Priddy <priddyjacob84@gmail.com>
2016-09-08 00:06:47 -0400

    9/8 1206

---
+++ servoAnimation.js
@@ -0,0 +1,20 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+
+	var servo = new five.Servo();
+	var animation = new.five.Animation(servo);
+	console.log("servo & animation set"); 
+
+
+	animation.enqueue({
+		duration: 2000,
+		cuePoints: [0, 0.25, 0.5, 0.75, 1.0],
+		keyFrames: [ {degrees: 0}, {degrees: 135}, {degrees: 45}, {degrees: 180}, {degrees: 0}]
+	});
+
+	//sweep from left to right
+	servo.sweep();
+	console.log("sweep complete");
+});
